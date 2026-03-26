ec29798dcfeae981358762a4fa3d411b6779485e
unknown <mdiperna.02@outlook.com>
2026-03-26 19:59:20 +0100

    project setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  {/* With <> inside the return we can write more than one <div>. Generally we can insert more than one parent*/}
+  return (
+    <> 
+      
+    </>
+  )
+}
+
+
+export default App
