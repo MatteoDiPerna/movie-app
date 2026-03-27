dde7a52988ecfc5c0af0a5dbe4e00c86a7e79322
unknown <mdiperna.02@outlook.com>
2026-03-27 23:30:45 +0100

    home & movieCard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
+import Home from './pages/Home'
 
 function App() {
+  const movieNumber=1;
+
   {/* With <> inside the return we can write more than one <div>. Generally we can insert more than one parent*/}
   return (
     <> 
-      
+      <Home />
     </>
   )
 }
