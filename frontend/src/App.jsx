c3419e678e2f576632d350d4af6a4536154fd7d4
unknown <mdiperna.02@outlook.com>
2026-03-28 15:07:25 +0100

    Adde css style for each component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,24 @@
-import './App.css'
-import Home from './pages/Home'
+import './css/App.css';
+import Home from './pages/Home';
+import Favorites from './pages/Favorites';
+import {Routes, Route} from 'react-router-dom';
+import NavBar from './components/NavBar';
 
 function App() {
   const movieNumber=1;
 
   {/* With <> inside the return we can write more than one <div>. Generally we can insert more than one parent*/}
   return (
-    <> 
-      <Home />
-    </>
+    <div>
+      <NavBar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Routes>
+      </main>
+    </div>
+
   )
 }
 
---
+++ frontend/src/pages/Favorites.jsx
@@ -0,0 +1,10 @@
+import "../css/Favorites.css";
+
+function Favourites() {
+    return <div className="favorites-empty">
+        <h2>No Favorite Yet</h2>
+        <p>Start adding movies to your favorites and they appear here</p>
+    </div>
+}
+
+export default Favourites
